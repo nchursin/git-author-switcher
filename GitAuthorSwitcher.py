d5499cf08b5683ff131d2c3a2fd1a774df96fb7a
Nikita Chursin <chursinn91@gmail.com>
2019-12-24 11:49:07 +0300

    add user btn

--- GitAuthorSwitcher.py
+++ GitAuthorSwitcher.py
@@ -1,11 +1,18 @@
 import rumps
 import os
 
+rumps.debug_mode = True
+
 class GitAuthorSwitcher(rumps.App):
     def __init__(self):
         super(GitAuthorSwitcher, self).__init__("Git Author Switcher")
-        self.menu = ["Nikita Aquiva", "Nikita Chursin", "Dmitriy Prozorovskiy"]
+        self.menu = ["Nikita Personal", "Nikita Aquiva", "Nikita Selas", "Dmitriy Prozorovskiy"]
 
+    @rumps.clicked("Nikita Personal")
+    def selectNikitaPersonal(self, sender):
+        self.disableAll()
+        sender.state = 1
+        self.selectUser('Nikita Chursin', 'chursinn91@gmail.com')
 
     @rumps.clicked("Nikita Aquiva")
     def selectNikitaAquiva(self, sender):
@@ -13,7 +20,7 @@ class GitAuthorSwitcher(rumps.App):
         sender.state = 1
         self.selectUser('Nikita Chursin', 'fake@fake.fake')
 
-    @rumps.clicked("Nikita Chursin")
+    @rumps.clicked("Nikita Selas")
     def selectNikita(self, sender):
         self.disableAll()
         sender.state = 1
@@ -25,6 +32,27 @@ class GitAuthorSwitcher(rumps.App):
         sender.state = 1
         self.selectUser('Dmitriy Prozorovskiy', 'fake@fake.fake')
 
+    @rumps.clicked("Add User")
+    def addUser(self, sender):
+        self.disableAll()
+        window = rumps.Window('Enter button name, username and email.\nEach must be on a new line', 'Add User', "", "Next", True)
+        response = window.run()
+        print(response)
+        if not response.clicked:
+            return
+
+        [buttonName, userName, email]  = response.text.split('\n')
+        if not (self.is_not_empty(buttonName) and self.is_not_empty(userName) and self.is_not_empty(email)):
+            return
+
+        userSelector = self.selectUserDec(userName, email)
+        newMenuItem = rumps.MenuItem(buttonName, callback=userSelector)
+        self.menu.insert_before("Add User", newMenuItem)
+
+
+    def is_not_empty(self, string):
+        return bool(string.strip())
+
     def selectUser(self, username, email):
         self.title = 'Git: ' + username
         userChangeCmd = 'git config --global user.name "{}"'.format(username)
@@ -32,10 +60,19 @@ class GitAuthorSwitcher(rumps.App):
         os.system(userChangeCmd)
         os.system(emailChangeCmd)
 
+    def selectUserDec(self, username, email):
+        that = self
+        def userSelector(self):
+            that.title = 'Git: ' + username
+            userChangeCmd = 'git config --global user.name "{}"'.format(username)
+            emailChangeCmd = 'git config --global user.email "{}"'.format(email)
+            os.system(userChangeCmd)
+            os.system(emailChangeCmd)
+        return userSelector
+
     def disableAll(self):
         print(self.menu.values)
         for item in self.menu.values():
-            print(item)
             item.state = 0
 
 if __name__ == "__main__":
