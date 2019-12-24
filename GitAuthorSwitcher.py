61443fb380768b557953a224576983bd787cb9fb
Nikita Chursin <chursinn91@gmail.com>
2019-12-24 12:27:03 +0300

    load user configs from file

--- GitAuthorSwitcher.py
+++ GitAuthorSwitcher.py
@@ -1,36 +1,58 @@
 import rumps
 import os
+import json
 
 rumps.debug_mode = True
+
+CONFIG_FILE_NAME = "userConfig.json"
 
 class GitAuthorSwitcher(rumps.App):
     def __init__(self):
         super(GitAuthorSwitcher, self).__init__("Git Author Switcher")
-        self.menu = ["Nikita Personal", "Nikita Aquiva", "Nikita Selas", "Dmitriy Prozorovskiy"]
+        self.menu = ["Add User"]
+        try:
+            with open(CONFIG_FILE_NAME) as config_file:
+                self.config_data = json.load(config_file)
+                for user_config in self.config_data:
+                    self.add_new_user_to_menu(user_config)
+        except IOError:
+            print("File not accessible")
+    
+    def create_menu_item(self, user_config):
+        if not self.is_user_config_valid(user_config):
+            return None
 
-    @rumps.clicked("Nikita Personal")
-    def selectNikitaPersonal(self, sender):
-        self.disableAll()
-        sender.state = 1
-        self.selectUser('Nikita Chursin', 'chursinn91@gmail.com')
+        buttonName = user_config["title"]
+        userName = user_config["username"]
+        email = user_config["email"]
 
-    @rumps.clicked("Nikita Aquiva")
-    def selectNikitaAquiva(self, sender):
-        self.disableAll()
-        sender.state = 1
-        self.selectUser('Nikita Chursin', 'fake@fake.fake')
+        userSelector = self.selectUserDec(userName, email)
+        newMenuItem = rumps.MenuItem(buttonName, callback=userSelector)
+        return newMenuItem
 
-    @rumps.clicked("Nikita Selas")
-    def selectNikita(self, sender):
-        self.disableAll()
-        sender.state = 1
-        self.selectUser('Nikita Chursin', 'fake@fake.fake')
+    def add_new_user_to_menu(self, user_config):
+        new_menu_item = self.create_menu_item(user_config)
+        if not new_menu_item is None:
+            self.menu.insert_before("Add User", new_menu_item)
 
-    @rumps.clicked("Dmitriy Prozorovskiy")
-    def selectDima(self, sender):
-        self.disableAll()
-        sender.state = 1
-        self.selectUser('Dmitriy Prozorovskiy', 'fake@fake.fake')
+    def add_user_config(self, title, username, email):
+        user_config = {
+            'title': title,
+            'username': username,
+            'email': email,
+        }
+        if not self.is_user_config_valid(user_config):
+            return None
+        self.config_data.append(user_config)
+        return user_config
+    
+    def is_user_config_valid(self, user_config):
+        if user_config is None:
+            return False
+        buttonName = user_config["title"]
+        userName = user_config["username"]
+        email = user_config["email"]
+        return self.is_not_empty(buttonName) and self.is_not_empty(userName) and self.is_not_empty(email)
 
     @rumps.clicked("Add User")
     def addUser(self, sender):
@@ -42,23 +64,15 @@ class GitAuthorSwitcher(rumps.App):
             return
 
         [buttonName, userName, email]  = response.text.split('\n')
-        if not (self.is_not_empty(buttonName) and self.is_not_empty(userName) and self.is_not_empty(email)):
-            return
+        user_config = self.add_user_config(buttonName, userName, email)
+        if user_config is None:
+            return None
 
-        userSelector = self.selectUserDec(userName, email)
-        newMenuItem = rumps.MenuItem(buttonName, callback=userSelector)
-        self.menu.insert_before("Add User", newMenuItem)
+        self.add_new_user_to_menu(user_config)
 
 
     def is_not_empty(self, string):
         return bool(string.strip())
-
-    def selectUser(self, username, email):
-        self.title = 'Git: ' + username
-        userChangeCmd = 'git config --global user.name "{}"'.format(username)
-        emailChangeCmd = 'git config --global user.email "{}"'.format(email)
-        os.system(userChangeCmd)
-        os.system(emailChangeCmd)
 
     def selectUserDec(self, username, email):
         that = self
