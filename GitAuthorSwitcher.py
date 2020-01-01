14f9e5be1b299261c1540437f094d1775eebeddb
Nikita Chursin <chursinn91@gmail.com>
2020-01-01 22:56:48 +0300

    read config on init to preselect user

--- GitAuthorSwitcher.py
+++ GitAuthorSwitcher.py
@@ -1,22 +1,52 @@
 import rumps
 import os
 import json
+from subprocess import PIPE, Popen, STDOUT
 
 rumps.debug_mode = True
 
 CONFIG_FILE_NAME = "userConfig.json"
 
 class GitAuthorSwitcher(rumps.App):
+    all_emails = []
+    selected_emails = []
+    user_data = []
+    
     def __init__(self):
         super(GitAuthorSwitcher, self).__init__("Git Author Switcher")
+        self.form_menu()
+            
+    def form_menu(self):
         self.menu = ["Add User"]
+        self.read_user_config()
+        selected_user_emails = self.get_current_user()
+        
+        self.build_menu(selected_user_emails)
+            
+    def build_menu(self, selected_emails):
+        for user_config in self.user_data:
+            is_selected = user_config['email'] in selected_emails
+            self.add_new_user_to_menu(user_config, is_selected)
+    
+    def read_user_config(self):
         try:
             with open(CONFIG_FILE_NAME) as config_file:
-                self.config_data = json.load(config_file)
-                for user_config in self.config_data:
-                    self.add_new_user_to_menu(user_config)
+                self.user_data = json.load(config_file)
+                for user_config in self.user_data:
+                    self.all_emails.append(user_config['email'])
         except IOError:
             print("File not accessible")
+            
+    def get_current_user(self): 
+        current_git_useremail = Popen(["git", "config", "--global", "user.email"], stdout=PIPE).communicate()[0].strip().decode("utf-8")
+        user_emails = list(map(lambda text: text.strip(), current_git_useremail.split(',')))
+        self.selected_emails = list(
+            filter(
+                lambda email: email in self.all_emails,
+                user_emails
+            )
+        )
+        return user_emails
     
     def create_menu_item(self, user_config):
         if not self.is_user_config_valid(user_config):
@@ -26,14 +56,16 @@ class GitAuthorSwitcher(rumps.App):
         userName = user_config["username"]
         email = user_config["email"]
 
-        userSelector = self.selectUserDec(userName, email)
-        newMenuItem = rumps.MenuItem(buttonName, callback=userSelector)
+        user_selector = self.select_user_dec(userName, email)
+        newMenuItem = rumps.MenuItem(buttonName, callback=user_selector)
         return newMenuItem
 
-    def add_new_user_to_menu(self, user_config):
+    def add_new_user_to_menu(self, user_config, is_selected):
         new_menu_item = self.create_menu_item(user_config)
         if not new_menu_item is None:
+            new_menu_item.state = int(is_selected)
             self.menu.insert_before("Add User", new_menu_item)
+            self.set_title_username(user_config['title'])
 
     def add_user_config(self, title, username, email):
         user_config = {
@@ -43,9 +75,9 @@ class GitAuthorSwitcher(rumps.App):
         }
         if not self.is_user_config_valid(user_config):
             return None
-        self.config_data.append(user_config)
+        self.user_data.append(user_config)
         with open(CONFIG_FILE_NAME, "w") as outfile:
-            json.dump(self.config_data, outfile)
+            json.dump(self.user_data, outfile)
         return user_config
     
     def is_user_config_valid(self, user_config):
@@ -75,16 +107,19 @@ class GitAuthorSwitcher(rumps.App):
 
     def is_not_empty(self, string):
         return bool(string.strip())
+    
+    def set_title_username(self, username): 
+        self.title = 'Git: ' + username
 
-    def selectUserDec(self, username, email):
+    def select_user_dec(self, username, email):
         that = self
-        def userSelector(self):
-            that.title = 'Git: ' + username
+        def user_selector(self):
+            that.set_title_username(username)
             userChangeCmd = 'git config --global user.name "{}"'.format(username)
             emailChangeCmd = 'git config --global user.email "{}"'.format(email)
             os.system(userChangeCmd)
             os.system(emailChangeCmd)
-        return userSelector
+        return user_selector
 
     def disableAll(self):
         print(self.menu.values)
